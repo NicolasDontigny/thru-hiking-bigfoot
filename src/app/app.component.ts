64ab976be4c29d725d2e698bdf003ef2527cb449
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2022-11-30 17:09:21 -0500

    Added global style folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pct-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
+  styleUrls: [
+    './app.component.less',
+    './styles/index.less',
+  ],
 })
 export class AppComponent {
   title = 'PCT-Resupply';
